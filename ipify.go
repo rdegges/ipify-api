8d43b00908eccb9dd4ae14d3ffaab5fb3b0ca77b
Randall Degges <r@rdegges.com>
2014-01-01 22:00:25 -0800

    Testing a Go app.

---
+++ ipify.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
+
+type IPAddress struct {
+	IP string `json:"ip"`
+}
+
+func ipify(w http.ResponseWriter, r *http.Request) {
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		log.Fatal("SplitHostPort:", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	jsonStr, _ := json.MarshalIndent(IPAddress{host}, "", "  ")
+	fmt.Fprintf(w, string(jsonStr))
+}
+
+func main() {
+	http.HandleFunc("/", ipify)
+
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
