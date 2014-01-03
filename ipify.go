4c7e6ec7b02c5a7cef76e1bea476c5875a344dbb
Randall Degges <r@rdegges.com>
2014-01-02 22:10:00 -0800

    Adding a /text endpoint which returns the IP as plain text.

--- ipify.go
+++ ipify.go
@@ -12,7 +12,7 @@ type IPAddress struct {
 	IP string `json:"ip"`
 }
 
-func ipify(w http.ResponseWriter, r *http.Request) {
+func jsonip(w http.ResponseWriter, r *http.Request) {
 	host := net.ParseIP(r.Header["X-Forwarded-For"][len(r.Header["X-Forwarded-For"])-1]).String()
 	jsonStr, _ := json.Marshal(IPAddress{host})
 
@@ -20,8 +20,16 @@ func ipify(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(jsonStr))
 }
 
+func textip(w http.ResponseWriter, r *http.Request) {
+	host := net.ParseIP(r.Header["X-Forwarded-For"][len(r.Header["X-Forwarded-For"])-1]).String()
+
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, host)
+}
+
 func main() {
-	http.HandleFunc("/", ipify)
+	http.HandleFunc("/json", jsonip)
+	http.HandleFunc("/text", textip)
 
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
