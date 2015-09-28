d9b44c02086f7e41eb1d38f9ff3fe87397a9ea7f
Randall Degges <r@rdegges.com>
2015-09-28 16:35:54 -0700

    Simplifying error handlers again.

--- api/error_handlers.go
+++ api/error_handlers.go
@@ -9,13 +9,13 @@ import (
 	"net/http"
 )
 
-// NotFound renders a not found response for invalid API endpoints.
-func NotFound(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(404)
-}
-
 // MethodNotAllowed renders a method not allowed response for invalid request
 // types.
 func MethodNotAllowed(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(405)
+}
+
+// NotFound renders a not found response for invalid API endpoints.
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
 }
